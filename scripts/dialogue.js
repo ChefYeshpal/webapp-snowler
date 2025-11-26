1cfde876ef558705d79f89423c0ed26306a54ac3
ChefYeshpal <choudharydhruva@gmail.com>
2025-11-26 13:24:28 +0530

    added dialogue

--- scripts/dialogue.js
+++ scripts/dialogue.js
@@ -3,5 +3,9 @@ window.dialogueLines = [
     'You brush the frost from your jacket as you pull yourself free, breath fogging in the brittle air.',
     'Looking around, you notice you are standing in a snowy pine forest, rows of dark trunks fading into white.',
     "It is quiet, the stillness broken only by the hush of wind pushing cold against your cheeks.",
-    'How did you even get here? You do not know...'
+    'How did you even get here? You do not know...',
+    'You look around, and all you see are the tall trees and the endless snow',
+    'There seems to be a path leading something into the forest... maybe it will lead you somewhere to more people?',
+    'Or, you could try to get a fire going here and wait for help to arrive...',
+    'What will you do?'
 ];
